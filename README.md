d5e919e7eb08b2800734d749c1cec8903f879400
cnouguier <christophe.nouguier@free.fr>
2021-11-09 14:47:26 +0100

    wip: first version

---
+++ README.md
@@ -0,0 +1,44 @@
+# k-centipede
+
+[![Latest Release](https://img.shields.io/github/v/tag/kalisio/k-centipede?sort=semver&label=latest)](https://github.com/kalisio/k-centipede/releases)
+[![Build Status](https://app.travis-ci.com/kalisio/k-centipede.svg?branch=master)](https://app.travis-ci.com/kalisio/k-centipede)
+
+A [Krawler](https://kalisio.github.io/krawler/) based service to get the antennas status of the [RTK Centipede Network](https://docs.centipede.fr/)
+
+
+## Description
+
+The **k-centipede** job allow to scrape antennas status from the [centipede RTK server](https://centipede.fr/). The downloaded data are stored within a [MongoDB](https://www.mongodb.com/) database and more precisely in 2 collections: 
+* the `bases` collection stores the data relative to the bases 
+* the `pings` collection stores the ping status of each base
+
+
+All records are stored in [GeoJson](https://fr.wikipedia.org/wiki/GeoJSON) format.
+
+The job is executed according a specific cron expression. By default, every 5 minutes.
+
+## Configuration
+
+| Variable | Description |
+|--- | --- |
+| `DB_URL` | The mongoDB database URL. The default value is `mongodb://127.0.0.1:27017/centipede` |
+| `TTL` | The measurements data time to live. It must be expressed in seconds and the default value is `604 800` (7 days) | 
+| `DEBUG` | Enables debug output. Set it to `krawler*` to enable full output. By default it is undefined. |
+
+## Deployment
+
+We personally use [Kargo](https://kalisio.github.io/kargo/) to deploy the service.
+
+## Contributing
+
+Please refer to [contribution section](./CONTRIBUTING.md) for more details.
+
+## Authors
+
+This project is sponsored by 
+
+![Kalisio](https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-256x84.png)
+
+## License
+
+This project is licensed under the MIT License - see the [license file](./LICENSE) for details
---
+++ jobfile.js
@@ -0,0 +1,138 @@
+const _ = require('lodash')
+
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/centipede'
+
+module.exports = {
+  id: 'centipede',
+  store: 'memory',
+  options: {
+    workersLimit: 1,
+    faultTolerant: true,
+  },
+  tasks: [{
+    id: 'basesrtk',
+    type: 'wfs',
+    options: {
+      url: 'https://centipede.fr/index.php/lizmap/service/?repository=cent&project=centipede&VERSION=1.3.0&SERVICE=WFS&REQUEST=GetFeature&typeName=basesrtk&outputformat=geojson'
+    }
+  }],
+  hooks: {
+    tasks: {
+      after: {
+        readJson: {},
+        apply: {
+          function: (item) => {
+            let bases = []
+            let pings = []
+            _.forEach(item.data.features, (feature) => {
+              let base = {
+                type: feature.type,
+                geometry: feature.geometry,
+                properties: _.pick(feature.properties, [
+                  'id',
+                  'mp',
+                  'data_format', 
+                  'systeme', 
+                  'frequence', 
+                  'recepteur', 
+                  'type_antenne', 
+                  'rapport',
+                  'receiver',
+                  'version',
+                  'antenne',
+                  'syscoor'
+                ])
+              }
+              if (feature.rapport) base.rapport = 'https://centipede.fr/index.php/view/media/' + feature.rapport
+              bases.push(base)
+              pings.push({
+                type: feature.type,
+                geometry: feature.geometry,
+                properties: {
+                  id: feature.properties.id,
+                  name: feature.properties.mp,
+                  ping: feature.properties.ping === 'active' ? feature.properties.rapport ? 2 : 1 : 0,
+                  message: feature.properties.rtcm_messages,
+                  key: feature.properties.id + '-' + feature.properties.check_activite,
+                },
+                time: feature.properties.check_activite
+              })
+            })
+            item.bases = bases
+            item.pings = pings
+          }
+        },
+        updateBasesCollection: {
+          hook: 'updateMongoCollection',
+          collection: 'centipede-bases',
+          dataPath: 'result.bases',
+          filter: { 'properties.id': '<%= properties.id %>' },
+          upsert : true,
+          transform: {
+            mapping: {
+              'properties.mp': 'properties.name'
+            }
+          },
+          chunkSize: 256
+        },
+        updatePingsCollection: {
+          hook: 'updateMongoCollection',
+          collection: 'centipede-pings',
+          dataPath: 'result.pings',
+          filter: { 'properties.key': '<%= properties.key %>' },
+          upsert : true,
+          transform: {
+            unitMapping: { 
+              time: { asDate: 'utc' } 
+            }
+          },
+          chunkSize: 256
+        },
+        clearData: {}
+      }
+    },
+    jobs: {
+      before: {
+        createStores: [{
+          id: 'memory'
+        }],
+        connectMongo: {
+          url: dbUrl,
+          // Required so that client is forwarded from job to tasks
+          clientPath: 'taskTemplate.client'
+        },
+        createBasesCollection: {
+          hook: 'createMongoCollection',
+          clientPath: 'taskTemplate.client',
+          collection: 'centipede-bases',
+          indices: [
+            [{ 'properties.id': 1 }, { unique: true }],
+            { geometry: '2dsphere' }
+          ]
+        },
+        createPingsCollection: {
+          hook: 'createMongoCollection',
+          clientPath: 'taskTemplate.client',
+          collection: 'centipede-pings',
+          indices: [
+            [{ time: 1, 'properties.id': 1 }, { unique: true }],
+            { 'properties.ping': 1 },
+            { geometry: '2dsphere' }
+          ]
+        }
+      },
+      after: {
+        disconnectMongo: {
+          clientPath: 'taskTemplate.client'
+        },
+        removeStores: ['memory']
+      },
+      error: {
+        disconnectMongo: {
+          clientPath: 'taskTemplate.client'
+        },
+        removeStores: ['memory']
+      }
+    }
+  }
+}
